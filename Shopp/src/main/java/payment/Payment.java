135deecfbb3180ed5431c10b89df09c5bf4ed561
MThaiTran <69384865+MThaiTran@users.noreply.github.com>
2024-11-04 23:14:43 +0700

    Delete Shopp directory

--- Shopp/src/main/java/payment/Payment.java
+++
@@ -1,9 +0,0 @@
-package payment;
-
-public class Payment {
-    public void processPayment(double amount) {
-        System.out.println("Processing payment of $" + amount);
-        // Thêm các logic liên quan đến thanh toán ở đây, ví dụ: xác thực thẻ, áp dụng voucher, v.v.
-        System.out.println("Payment successful!");
-    }
-}
